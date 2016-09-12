c7e5ea8979d7ffbc4ec85889aa0aac56002dce3f
Cristofher <cristofherrojas.cr@gmail.com>
2016-09-12 04:23:52 -0300

    Maqueta

---
+++ default/app/controllers/muebleria_controller.php
@@ -0,0 +1,17 @@
+<?php
+class MuebleriaController extends AppController {
+	    public function login()
+    {
+        
+    }
+    	public function home()
+    {
+        
+    }
+    public function plane_blank()
+    {
+    	# code...
+    }
+
+
+}
