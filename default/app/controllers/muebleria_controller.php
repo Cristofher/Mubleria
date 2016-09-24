e3fcd80fc98f995e3ce48e2916b397d4baee592a
Cristofher <cristofherrojas.cr@gmail.com>
2016-09-24 04:24:08 -0300

    Login funcionando y cerrar sesión Creación modelo usuario y configuración base de dato Mejorar al controler Login se cambiaron los input por los del helper Form de kumbiaphp

--- default/app/controllers/muebleria_controller.php
+++ default/app/controllers/muebleria_controller.php
@@ -2,15 +2,37 @@
 class MuebleriaController extends AppController {
 	    public function login()
     {
-        
+        if (Input::hasPost("login","password")){
+            $pwd = Input::post("password");
+            $usuario=Input::post("login");
+ 
+            $auth = new Auth("model", "class: usuarios", "user: $usuario", "password: $pwd");
+            if ($auth->authenticate()) {
+                Router::redirect("muebleria/home");
+            } else {
+                Flash::error("Falló");
+            }
+        }
     }
     	public function home()
     {
-        
+        if (!Auth::is_valid()) {
+            // El usuario no es valido, lo mandamos al login
+            Redirect::to("muebleria/login"); 
+        }
+    }
+
+    public function salir()
+    {
+        Auth::destroy_identity();
+        Redirect::to("muebleria/login");
     }
     public function plane_blank()
     {
-    	# code...
+    	if (!Auth::is_valid()) {
+            // El usuario no es valido, lo mandamos al login
+            Redirect::to("muebleria/login"); 
+        }
     }
 
 
